314abe58472ec5bf256c4b0805c146d4e7c54507
Patrick Scanlan <patscan@gmail.com>
2012-06-27 01:35:28 -0700

    finished up to 12.2.1 - resume there

---
+++ app/models/relationship.rb
@@ -0,0 +1,10 @@
+class Relationship < ActiveRecord::Base
+  attr_accessible :followed_id
+  # only who is being followed by you is editable
+  
+  belongs_to :follower, :class_name => "User"
+  belongs_to :followed, :class_name => "User"
+  
+  validates :follower_id, :presence => true
+  validates :followed_id, :presence => true
+end
