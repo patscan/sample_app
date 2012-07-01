e545fe2c0ee720b3405529f7aec68082571e64c3
Patrick Scanlan <patscan@gmail.com>
2012-07-01 03:59:28 -0700

    Added user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,13 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
   
   has_many :relationships, :foreign_key => "follower_id",
                            :dependent => :destroy
@@ -46,8 +53,7 @@ class User < ActiveRecord::Base
   before_save :encrypt_password
   
   def feed
-    # This is a preliminary. See Chapter 12 for full implementation
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
   
   #Return true if the user's password matches the submitted password.
@@ -79,7 +85,6 @@ class User < ActiveRecord::Base
   def unfollow!(followed)
     relationships.find_by_followed_id(followed).destroy
   end
-    
   
   private
   
