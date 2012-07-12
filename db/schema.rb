be5275c8b8b806dc8bc6ce6270d72b3002f467e0
Patrick Scanlan <patscan@gmail.com>
2012-07-12 01:26:01 -0700

    added username column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120627064016) do
+ActiveRecord::Schema.define(:version => 20120712082100) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20120627064016) do
     t.string   "encrypted_password"
     t.string   "salt"
     t.boolean  "admin"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
