4d0607c9441999d65b6f8b27a280a3c2805c9b85
Patrick Scanlan <patscan@gmail.com>
2012-07-20 02:53:48 -0700

    testing for replies improved, :including_replies scope fixed

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,6 @@
 class Micropost < ActiveRecord::Base
+  
+  attr_accessor :in_reply_to
   attr_accessible :content
   
   belongs_to :user
@@ -12,12 +14,13 @@ class Micropost < ActiveRecord::Base
   scope :from_users_followed_by, lambda { |user| followed_by(user) }
   
   # Return microposts addressed to the user
-  # scope :including_replies
+  scope :including_replies, where(:in_reply_to => :user_id)
+  
   
   private
   
   # Return an SQL condition for users followed by the given user.
-  # We include the uder's own id as well.
+  # We include the uder's own id as well. CHANGE THIS TO AREL
   
   def self.followed_by(user)
     following_ids = %(SELECT followed_id FROM relationships
@@ -25,6 +28,10 @@ class Micropost < ActiveRecord::Base
     where("user_id IN (#{following_ids}) OR user_id = :user_id",
           { :user_id => user })
   end
+  
+  #def self.addressed_to(user)
+  #  addressed_ids = Micropost.select()
+  #end
 
   # This might involve adding an in_reply_to column in
   # the microposts table and an extra including_replies
